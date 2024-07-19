5f2e44aed4ac58bb1642d873fe7872630862b1bd
Aishumph <aishwarya.rajagopal@mphasis.com>
2024-07-19 17:52:43 +0530

    fullscreen added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { FileComponent } from './file/file.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'file', component: FileComponent},
+{path: 'edit', component: ContentComponent},
+{path: 'view', component: ContentComponent},
+{path: 'go', component: ContentComponent},
+{path: 'tools', component: ContentComponent},
+{path: 'help', component: ContentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// Material Modules
+import { MatIconModule  } from '@angular/material/icon';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import{ MatButtonModule} from '@angular/material/button';
+import{MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileComponent } from './file/file.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FileComponent,
+    SidenavComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
